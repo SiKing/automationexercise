dbe048b65d9ee569f7c04027a978f434b4eff8f1
V. Mark Lehky <mark.lehky@gmail.com>
2025-06-13 08:20:30 -0700

    rename class

--- src/test/java/com/automationexercise/selenium/LaunchBrowser.java
+++ src/test/java/com/automationexercise/selenium/LaunchBrowser.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public class CustomOptions {
+public class LaunchBrowser {
 
     static WebDriver driver;
 
